fa8d57cef300f267e7638f934544d99deb02b52e
Arthur Carneiro <arthur.menezes@presidencia.gov.br>
2021-03-10 16:18:42 -0300

    Servindo HTML estatico

--- 01-github-explorer/webpack.config.js
+++ 01-github-explorer/webpack.config.js
@@ -1,5 +1,6 @@
 const { resolve } = require('path')
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
@@ -11,12 +12,17 @@ module.exports = {
   resolve: {
     extensions: ['.jsx', '.js'],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+    }),
+  ],
   module: {
     rules: [
       {
         test: /\.jsx$/,
         exclude: /node_modules/,
-        use: 'babel-loader'
+        use: 'babel-loader',
       },
     ],
   },
